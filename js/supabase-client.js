264bb90db9e717423062fb7d04c9b234dcf1ba31
sarthaksaxena991-tech <sarthaksaxena991@gmail.com>
2025-09-06 12:28:15 +0530

    Update supabase-client.js

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,3 +1,4 @@
 window.SUPABASE_URL = 'https://vdbjltfyoxmiijuwjlur.supabase.co';
 window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkYmpsdGZ5b3htaWlqdXdqbHVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI5MTA5MDcsImV4cCI6MjA2ODQ4NjkwN30.rSHfuHf2VcpEua__z2GXipSHVs_3gWSayfswIOyNL9E';
 window.supabaseClient = supabase.createClient(window.SUPABASE_URL, window.SUPABASE_ANON_KEY);
+id="alert";
